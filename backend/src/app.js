458bbc817712c6d50b503f79a69b8837d3a3b5ec
Nicolas <Nicolas.albagualteros@gmail.com>
2023-09-15 15:52:53 -0500

    PETICION $

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send ('peticion GET');
+app.get('/:id', (req, res) =>{
+    const{ id }= req.params
+    res.send (`peticion GET ${id}`);
 });
 
 app.post('/', (req, res) =>{
