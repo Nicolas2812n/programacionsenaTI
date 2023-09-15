1a6f9b0929084eaaa5321d1615a420c4dff8613d
Nicolas <Nicolas.albagualteros@gmail.com>
2023-09-15 14:39:45 -0500

    peticion get

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-    // res.send ('Hola mundo');
-// });
+app.get('/', (req, res) =>{
+    res.send ('Hola mundo');
+});
 
 app.listen(3000, () => {
     console.log ('servidor encendido');
