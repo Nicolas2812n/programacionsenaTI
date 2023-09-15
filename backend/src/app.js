06f5030ef7c8a1663210d9a65582d5b793b0aa95
Nicolas <Nicolas.albagualteros@gmail.com>
2023-09-15 15:01:41 -0500

    peticion delete y peticion put

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send ('Hola mundo');
+    res.send ('peticion GET');
+});
+
+app.post('/', (req, res) =>{
+    res.send('peticion POST')
+});
+
+app.put('/', (req, res) =>{
+    res.send ('peticion PUT');
+});
+
+app.delete('/', (req, res) =>{
+    res.send ('peticion DELETE');
 });
 
 app.listen(3000, () => {
